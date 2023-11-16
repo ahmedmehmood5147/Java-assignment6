e08e26ea9cb54451717b28036a6440dd89be4a77
ahmedmehmood5147 <am9474036@gmail.com>
2023-11-17 03:14:21 +0500

    javaAssignment-6

---
+++ app.js
@@ -0,0 +1,64 @@
+document.write ("Task1" + " " + "Result")
+var a = 10;
+document.write ("<br>" + "The value of a is:" + " " + a)
+var a = ++a;
+document.write ("<br>" + "The value of ++a is:" + " " + a )
+document.write ("<br>" + "Now The value of a is:" + " " + a)
+var a = a++;
+document.write ("<br>" + "The value of a++ is:" + " " + a++ )
+document.write ("<br>" + "The value of a++ is:" + " " + a )
+var a = --a;
+document.write ("<br>" + "The value of --a is:" + " " + a )
+document.write ("<br>" + "Now The value of a is:" + " " + a)
+var a = a--;
+document.write ("<br>" + "The value of a-- is:" + " " + a-- )
+document.write ("<br>" + "The value of a-- is:" + " " + a )
+document.write ("<br>" + "Task2" + " " + "Result")
+var a = 2;
+var b = 1;
+           1  -  0  +  1  +  1
+var result = --a - --b + ++b + b--;
+document.write ("<br>" + result)
+var a = 2;
+var b = 1;
+var result = 
+// 1;
+//0 - 0;
+// -1 - -1 + 0;
+// -2 - -1 + 0 + 0
+
+--a;
+--a - --b;
+--a - --b + ++b;
+--a - --b + ++b + b--;
+document.write ("<br>" + "a is" + a)
+document.write ("<br>" + "b is" + b)
+document.write ("<br>" + "result is" + result)
+var name = prompt ("What is your name")
+document.write ("<br>" + "Task 2" + " " + name)
+var number = prompt ("Plase input a number")
+document.write ("<br>" + "Task 5" + "<br>" + number + " " + "x" + " " + "1" + " " + "=" + " " + number*1)
+document.write ("<br>" + number + " " + "x" + " " + "2" + " " + "=" + " " + number*2)
+document.write ("<br>" + number + " " + "x" + " " + "3" + " " + "=" + " " + number*3)
+document.write ("<br>" + number + " " + "x" + " " + "4" + " " + "=" + " " + number*4)
+document.write ("<br>" + number + " " + "x" + " " + "5" + " " + "=" + " " + number*5)
+document.write ("<br>" + number + " " + "x" + " " + "6" + " " + "=" + " " + number*6)
+document.write ("<br>" + number + " " + "x" + " " + "7" + " " + "=" + " " + number*7)
+document.write ("<br>" + number + " " + "x" + " " + "8" + " " + "=" + " " + number*8)
+document.write ("<br>" + number + " " + "x" + " " + "9" + " " + "=" + " " + number*9)
+document.write ("<br>" + number + " " + "x" + " " + "10" + " " + "=" + " " + number*10)
+document.write ("<br>" + "Task 6")
+
+var totalMarks = 100;
+document.write ("<br>" + "Your Total mark is" + " " + totalMarks)
+var subject = prompt ("Please enter subject")
+document.write ("<br>" + "Your subject is" + " " + subject)
+var obtainedMarks = prompt ("Please enter obtainedmarks")
+document.write ("<br>" + "Your obtained mark is" + " " +obtainedMarks)
+var per = obtainedMarks * totalMarks / 100
+document.write ("<br>" + "Your percentage is" + " " +per)
+
+
+
+
+
